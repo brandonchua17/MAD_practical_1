e39ded76818e14f24aed04518fe0351b439f8c1f
TARUC <brandonchua17@gmail.com>
2020-03-04 10:09:55 +0800

    third commit

--- app/src/main/java/com/example/mad_practical_1/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_1/MainActivity.kt
@@ -3,23 +3,28 @@ package com.example.mad_practical_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage = findViewById(R.id.dice_image)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countUpButton: Button = findViewById(R.id.countUp_button)
-        countUpButton.setOnClickListener { countUp() }
+        //val countUpButton: Button = findViewById(R.id.countUp_button)
+        //countUpButton.setOnClickListener { countUp() }
     }
 
-    private fun rollDice() {
+    /*private fun rollDice() {
         val resultText: TextView = findViewById(R.id.result_text)
 
         val randomInt = (1..6).random()
@@ -28,9 +33,27 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
+    }*/
+
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
     }
 
-    private fun countUp() {
+    /*private fun countUp() {
         val resultText: TextView = findViewById(R.id.result_text)
         val num = resultText.text.toString().toIntOrNull()
 
@@ -39,5 +62,19 @@ class MainActivity : AppCompatActivity() {
         else if(num < 6)
             resultText.text = (num + 1).toString()
 
-    }
+        // If text is the default "Hello World!" set that text to 1.
+        /*if (resultText.text == "Hello World!") {
+            resultText.text = "1"
+        } else {
+            // Otherwise, increment the number up to 6.
+            // The text value in resultText.text is an instance of the CharSequence class;
+            // it needs to be converted to a String object before it can be converted to an int.
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }*/
+    }*/
 }
